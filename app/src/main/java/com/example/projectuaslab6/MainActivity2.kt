e4f6070104e698d61b266d67427aa85c8296237e
Lenovo <rizkiamalina1984@gmail.com>
2024-10-30 22:38:08 +0700

    Commit 1

---
+++ app/src/main/java/com/example/projectuaslab6/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.yourappname
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectuaslab6.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2) // Set to the updated XML layout file
+    }
+}
