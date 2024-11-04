e80d4a515bc68d2c247ab66cb5080b7da58bb55e
Kyla Zahra Winnetou <kylazahraw@gmail.com>
2024-11-04 17:27:23 +0700

    Backend Splashsc-Login-Register-Home

--- app/src/main/java/com/example/projectuaslab6/MainActivity2.kt
+++ app/src/main/java/com/example/projectuaslab6/MainActivity2.kt
@@ -1,12 +1,23 @@
-package com.example.yourappname
+package com.example.projectuaslab6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectuaslab6.R
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2) // Set to the updated XML layout file
+        setContentView(R.layout.activity_main2)
+
+        val registerButton: Button = findViewById(R.id.registerButton)
+        registerButton.setOnClickListener {
+            // Lakukan pendaftaran, lalu kembali ke halaman login
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
+
