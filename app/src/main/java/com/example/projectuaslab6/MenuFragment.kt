05ee3f311a02c2fa7b7d166f60472ad75552ea7e
Kyla Zahra Winnetou <kylazahraw@gmail.com>
2024-11-13 21:51:49 +0700

    Benerin kode

--- app/src/main/java/com/example/projectuaslab6/MenuFragment.kt
+++ app/src/main/java/com/example/projectuaslab6/MenuFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -17,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -46,7 +45,6 @@ class MenuFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment MenuFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MenuFragment().apply {
