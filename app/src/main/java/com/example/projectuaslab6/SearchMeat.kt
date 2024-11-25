ed4b94140766a74c3c389963aa4eaaaf38a8e076
Lenovo <rizkiamalina1984@gmail.com>
2024-11-25 16:26:56 +0700

    Commit Rzki Amalina 8

---
+++ app/src/main/java/com/example/projectuaslab6/SearchMeat.kt
@@ -0,0 +1,20 @@
+package com.example.projectuaslab6
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SearchMeat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_search_meat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
