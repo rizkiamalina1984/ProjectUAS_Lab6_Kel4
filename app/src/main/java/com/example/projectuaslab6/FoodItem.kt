fe86b671f364fc1a6018c656b165e9f9b3391232
Kyla Zahra Winnetou <kylazahraw@gmail.com>
2024-12-02 23:11:12 +0700

    Penyempurnaan hasil akhir

--- app/src/main/java/com/example/projectuaslab6/FoodItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.projectuaslab6
-
-// Data class untuk menyimpan informasi tentang item makanan
-data class FoodItem(
-    val name: String,           // Nama makanan
-    val description: String,     // Deskripsi makanan
-    val tags: List<String>       // Daftar tag untuk makanan (misalnya "Meat", "Vegetables")
-)
